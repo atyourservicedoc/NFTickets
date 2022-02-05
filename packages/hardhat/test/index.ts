a144a306111f1c939b0f1012ba4dd168824deb59
nicolasguasca1 <nicolas.guasca@gmail.com>
2022-02-05 03:36:50 +0100

    +2

--- packages/hardhat/test/index.ts
+++ packages/hardhat/test/index.ts
@@ -21,24 +21,37 @@ describe("NFTMarket", function () {
     listingPrice = listingPrice.toString();
 
     const auctionPrice = ethers.utils.parseUnits("1", "ether");
+    const itemsCount = 1000;
+    const eventMock = "My frist event";
+    const brandMock = "LTK";
 
-    await nft.createToken("https://www.mytokenlocation.com");
-    await nft.createToken("https://www.mytokenlocation2.com");
+    await nft.createToken("https://www.mytokenlocation.com", "");
+    await nft.createToken("https://www.mytokenlocation2.com", "");
 
-    await market.createMarketItem(nftContractAddress, 1, auctionPrice, {
-      value: listingPrice
-    });
-    await market.createMarketItem(nftContractAddress, 2, auctionPrice, {
-      value: listingPrice
-    });
+    await market.createEventItem(
+      nftContractAddress,
+      1,
+      auctionPrice,
+      itemsCount,
+      eventMock,
+      brandMock
+    );
+    await market.createEventItem(
+      nftContractAddress,
+      2,
+      auctionPrice,
+      itemsCount,
+      eventMock,
+      brandMock
+    );
 
     const [_, buyerAddress] = await ethers.getSigners();
 
     await market
       .connect(buyerAddress)
-      .createMarketSale(nftContractAddress, 1, { value: auctionPrice });
+      .createEventSale(nftContractAddress, 1, itemsCount);
 
-    let items = await market.fetchMarketItems();
+    const items = await market.fetchEventItems();
 
     items = await Promise.all(
       items.map(async (i: item) => {
