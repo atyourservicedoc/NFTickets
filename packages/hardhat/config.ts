3b2eb9ee254d57822241824838700b726a140605
nicolasguasca1 <nicolas.guasca@gmail.com>
2022-02-07 00:52:04 +0100

    +1

--- packages/hardhat/config.ts
+++ packages/hardhat/config.ts
@@ -1,3 +1,5 @@
 export const event = "0xb2b02E177d42C5a7cE2d48b169Cab66AbBe2D87e";
+
+export const proxyevent = "0x46D0605C419E289Dd572169BdC44e32f0a784ABe";
 export const eventMarketplace = "0x41a34CF0C0f91F3E0Ff26B5a6b33d8B854B4b56E";
 export const concertMarketPlace = "0x8cCF83aE2ffA68b94eFe521D16D03D8D045B9995";
