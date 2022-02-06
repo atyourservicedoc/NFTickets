fe24e9be785ce40f6478b8a4348da70e92821668
Diane <diane@yourservice.network>
2022-02-06 15:41:31 +0000

    feat(contracts): fleshed out contracts

--- backend/hardhat/contracts/ConcertMarketPlaceWimports.sol
+++ backend/hardhat/contracts/ConcertMarketPlaceWimports.sol
@@ -1379,6 +1379,9 @@ pragma solidity ^0.8.4;
 
 //import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 //import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+//import counters
+//import safemath
+//import IERCstuff
 
 contract ConcertMarketPlace is ERC1155, ReentrancyGuard, Ownable {
     using Counters for Counters.Counter;
@@ -1390,7 +1393,6 @@ contract ConcertMarketPlace is ERC1155, ReentrancyGuard, Ownable {
     
     uint256 ticketId;
     Counters.Counter private _itemIds;
-    Counters.Counter private supplyCount;
     
 
     struct EventObject {
@@ -1398,13 +1400,21 @@ contract ConcertMarketPlace is ERC1155, ReentrancyGuard, Ownable {
         string name;
         string description;
         uint256 totalSupply;
+        uint256 supplyLeft;
         address owner;
         uint256 pricePerTicket;
         uint256 maxMintAmount;
         string baseURI;
     }
 
+    struct SalesObject {
+        uint256 withdrawnSoFar;
+        uint256 totalRevenue;
+    }
+
     mapping(uint256 => EventObject) private idToEventObject;
+    mapping(uint256 => Counters.Counter) private idToSupplyCount;
+    mapping(uint256 => SalesObject) private idToSalesObject
     //buyers mapping (userAddress, eventId => totalTicketsBought)
     //Event[] public events;
 
@@ -1453,8 +1463,12 @@ contract ConcertMarketPlace is ERC1155, ReentrancyGuard, Ownable {
     )
         external payable nonReentrant
     {
+        require(name.length > 0, "Event must have name");
+        require(description.length > 0, "Event must have description");
+        require(totalSupply > 0, "Event must have 1 ticket to sell");
         require(pricePerTicket > 0, "Price must be at least 1 wei");
-        require(msg.value == fee, "Price to set must be equal to listing price");
+        require(maxMintAmount > 0, "Max tickets per order must be at least 1");
+        require(msg.value == fee, "Must send create event fee at exact amount);
         _itemIds.increment();
         uint256 itemId = _itemIds.current();
         EventObject memory newEvent = EventObject(
@@ -1468,7 +1482,13 @@ contract ConcertMarketPlace is ERC1155, ReentrancyGuard, Ownable {
             baseURI
         );
 
+        SalesObject memory newSales = SalesObject(
+            0,
+            0
+        )
+
         idToEventObject[itemId] = newEvent;
+        idToSupplyCount[itemId] = salesObject;
         emit EventObjectCreated (
             itemId, 
             name,
@@ -1479,34 +1499,35 @@ contract ConcertMarketPlace is ERC1155, ReentrancyGuard, Ownable {
             maxMintAmount,
             baseURI
         );
-        //events[events.length+1] = {params from input, owner: msg.sender} //?
     }
 
 //THIS FUNCTION DOESN"T WORK, FINISH REVERT ERROR MESSAGES TO FIND OUT WHY
     function buyTicket(uint256 id, uint amount) external payable nonReentrant {
-        emit purchaseStarted (id, amount, msg.sender);
+        /*emit purchaseStarted (id, amount, msg.sender);
         if (amount < 0 || amount >= idToEventObject[id].maxMintAmount || supplyCount.current().add(amount) >= idToEventObject[id].totalSupply) {
             emit purchaseFailed (id, amount, msg.sender);
-        }
+        }*/
         require(amount > 0, "amount gotta be larger than 0");
-        require(amount <= idToEventObject[id].maxMintAmount);
-        require(supplyCount.current().add(amount) <= idToEventObject[id].totalSupply);
-        require(msg.value == (idToEventObject[id].pricePerTicket * amount)); // Should I add this to if statement?
+        require(amount <= idToEventObject[id].maxMintAmount, "can't buy more tickets than maxmintAmount");
+        require(supplyCount.current().add(amount) <= idToEventObject[id].totalSupply, "purchase would buy more tickets than exist");
+        require(msg.value == (idToEventObject[id].pricePerTicket * amount), "sent wrong amount of funds for minting tickets"); // Should I add this to if statement?
         _mint(msg.sender, id, amount, "");
-        //buyers(userAddress, eventId) += amount
-        //salesObject(id) = ticketsSold + amount, revenue + amount * (ticketPrice)
+        for (uint256 i = 0; i < amount; i++) {
+            idToSupplyCount[id].increment();
+        }
+        idToSalesObject[id].totalRevenue += msg.value;
         emit purchaseComplete(id, amount, msg.sender, idToEventObject[id].pricePerTicket);
     }
 
 
-    //function withdrawFunds(id, withdrawAmount) external payable nonReentrant {
-     //   require (idToEventObject[id].owner == msg.sender);
-        
-        //require withdrawn + withdrawAmount <= 
-        //salesObject[id] = withdrawn + withdrawnAmount
+    function withdrawFunds(id, withdrawAmount) external payable nonReentrant {
+        require (idToEventObject[id].owner == msg.sender, "Must be event creator");
+        require (salesObject[id].totalRevenue >= salesObject[id].withdrawnSoFar + withdrawAmount, "Trying to withdraw more than you've made")
+        salesObject[id] = withdrawn + withdrawnAmount
         //transfer(withdrawAmount)
+        idToSalesObject[id].withdrawnSoFar += withdrawAmount
         //it's 2am I'll finish this tmr
-    //}
+    }
 
     
 }
